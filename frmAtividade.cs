a17ff2cb5b7c936561625b1af84363b4b7e1b7c5
Willian Farias <williansantos_141@hotmail.com>
2020-07-07 05:20:27 -0300

    Update frmAtividade.cs

--- frmAtividade.cs
+++ frmAtividade.cs
@@ -45,7 +45,7 @@ namespace Atividade
 
         private void ExecutaAtividade(string filePath)
         {
-            this.Invoke(new MethodInvoker(delegate()
+            this.Invoke(new MethodInvoker(delegate ()
             {
                 txtArquivo.Enabled = false;
                 btnRun.Enabled = false;
@@ -55,21 +55,21 @@ namespace Atividade
             {
                 CodigoAtividade(filePath);
 
-                this.Invoke(new MethodInvoker(delegate()
+                this.Invoke(new MethodInvoker(delegate ()
                 {
                     MessageBox.Show(this, "Finalizado!");
                 }));
             }
             catch (Exception ex)
             {
-                this.Invoke(new MethodInvoker(delegate()
+                this.Invoke(new MethodInvoker(delegate ()
                 {
                     MessageBox.Show(this, ex.Message);
                 }));
             }
             finally
             {
-                this.Invoke(new MethodInvoker(delegate()
+                this.Invoke(new MethodInvoker(delegate ()
                 {
                     txtArquivo.Enabled = true;
                     btnRun.Enabled = true;
@@ -82,155 +82,286 @@ namespace Atividade
             // CÓDIGO AQUI!!
             try
             {
+                int lines, columns;
                 // ler o arquivo texto de entrada
-                using (StreamReader sr = File.OpenText(filePath))
+                StreamReader sr = File.OpenText(filePath);
+
+                // ler e salva a dimensão da matriz do labirinto
+                string[] vector = sr.ReadLine().Split(' ');
+                lines = int.Parse(vector[0]);
+                columns = int.Parse(vector[1]);
+
+                Maze maze1 = new Maze(lines, columns);
+
+                // ler e salva o labirinto em si
+                for (int l = 1; l <= lines; l++)
                 {
-                    // ler e salva a dimensão da matriz do labirinto
-                    string[] vector = sr.ReadLine().Split(' ');
-                    int lines = int.Parse(vector[0]);
-                    int columns = int.Parse(vector[1]);
-
-                    // ler e salva o labirinto em si
-                    Dictionary<string, string> maze = new Dictionary<string, string>();
-                    for (int l = 1; l <= lines; l++)
+                    string[] vector2 = sr.ReadLine().Split(' ');
+                    for (int c = 1; c <= columns; c++)
                     {
-                        string[] vector2 = sr.ReadLine().Split(' ');
-                        for (int c = 1; c <= columns; c++)
-                        {
-                            maze[l + "," + c] = vector2[c-1];
-                        }
+                        Position positionAux = new Position(l, c);
+                        maze1.AddComponent(positionAux, new MazeComponent(positionAux, maze1, vector2[c - 1]));
                     }
+                }
 
-                    // salva as posições visitadas
-                    List<string> visitedPositions = new List<string>();
-                    List<string> auxRetorno = new List<string>();
+                MazeChallenge mazeChallenge = new MazeChallenge(maze1);
 
-                    // salva a posição atual
-                    int currentPositionLine = 0;
-                    int currentPositionColumn = 0;
+                // verifica se no labirinto existe algum ponto de origem (X)
+                mazeChallenge.IdentifyOriginPosition();
 
-                    // verifica se no labirinto existe o ponto de origem (X)
-                    if (maze.ContainsValue("X"))
+                // tenta encontrar a saída
+                while (!mazeChallenge.ExitFound)
+                {
+                    mazeChallenge.ExecuteMovement();
+                }
+
+                // cria um arquivo para gravar a saída
+                string outputFilePath = Path.GetDirectoryName(filePath) + @"\saida-" + Path.GetFileName(filePath);
+                using (StreamWriter sw = File.CreateText(outputFilePath))
+                {
+                    foreach (var item in mazeChallenge.VisitedPositions)
                     {
-                        // identifica e salva a posição de origem
-                        for (int l = 1; l <= lines; l++)
-                        {
-                            for (int c = 1; c <= columns; c++)
-                            {
-                                if (maze[l + "," + c].Equals("X"))
-                                {
-                                    currentPositionLine = l;
-                                    currentPositionColumn = c;
-                                    visitedPositions.Add("O [" + l + ", " + c + "]");
-                                }
-                            }
-                        }
-                        string nextPosition = "", previousPosition = "O";
-                        int countPossibleMovements;
-                        do
-                        {
-                            // testa se pode deslocar para baixo
-                            countPossibleMovements = 0;
-                            if ((currentPositionLine + 1) > 0)
-                            {
-                                if (maze[(currentPositionLine + 1) + "," + currentPositionColumn].Equals("0") && !previousPosition.Equals("C"))
-                                {
-                                    nextPosition = "B";
-                                    countPossibleMovements++;
-                                }
-                            }
-                            // testa se pode deslocar para direita
-                            if ((currentPositionColumn + 1) > 0)
-                            {
-                                if (maze[currentPositionLine + "," + (currentPositionColumn + 1)].Equals("0") && !previousPosition.Equals("E"))
-                                {
-                                    nextPosition = "D";
-                                    countPossibleMovements++;
-                                }
-                            }
-                            // testa se pode deslocar para esquerda
-                            if ((currentPositionColumn - 1) > 0)
-                            {
-                                if (maze[currentPositionLine + "," + (currentPositionColumn - 1)].Equals("0") && !previousPosition.Equals("D"))
-                                {
-                                    nextPosition = "E";
-                                    countPossibleMovements++;
-                                }
-                            }
-                            // testa se pode deslocar para cima
-                            if ((currentPositionLine - 1) > 0)
-                            {
-                                if (maze[(currentPositionLine - 1) + "," + currentPositionColumn].Equals("0") && !previousPosition.Equals("B"))
-                                {
-                                    nextPosition = "C";
-                                    countPossibleMovements++;
-                                }
-                            }
-                            if (countPossibleMovements == 0)
-                            {
-                                nextPosition = auxRetorno.Last().Substring(0, 1);
-                                string[] vector3 = auxRetorno.Last().Substring(3,4).Split(',');
-                                currentPositionLine = int.Parse(vector3[0]);
-                                currentPositionColumn = int.Parse(vector3[1].Trim());
-                                auxRetorno.Remove(auxRetorno.Last());
-                                switch (nextPosition)
-                                {
-                                    case "C":
-                                        nextPosition = "B";
-                                        break;
-                                    case "E":
-                                        nextPosition = "D";
-                                        break;
-                                    case "D":
-                                        nextPosition = "E";
-                                        break;
-                                    case "B":
-                                        nextPosition = "C";
-                                        break;
-                                }
-                            }
-                            // faz o deslocamento
-                            switch (nextPosition)
-                            {
-                                case "C":
-                                    currentPositionLine--;
-                                    break;
-                                case "E":
-                                    currentPositionColumn--;
-                                    break;
-                                case "D":
-                                    currentPositionColumn++;
-                                    break;
-                                case "B":
-                                    currentPositionLine++;
-                                    break;
-                            }
-                            visitedPositions.Add(nextPosition + " [" + currentPositionLine + ", " + currentPositionColumn + "]");
-                            if (countPossibleMovements != 0)
-                            {
-                                auxRetorno.Add(nextPosition + " [" + currentPositionLine + ", " + currentPositionColumn + "]");
-                            }
-                            maze[currentPositionLine + "," + currentPositionColumn] = "1";
-                            previousPosition = nextPosition;
-                        }
-                        while (currentPositionColumn != columns && currentPositionColumn != 1 && currentPositionLine != lines && currentPositionLine != 1);
-
-                        foreach(var item in visitedPositions)
-                        {
-                            Console.WriteLine(item);
-                        }
-                        
-                    }
-                    else
-                    {
-                        Console.WriteLine("O labirinto não possui ponto de origem (X).");
+                        sw.WriteLine(item);
                     }
                 }
             }
-            catch(IOException e)
+            catch (IOException e)
             {
                 Console.WriteLine("An error occurred.");
                 Console.WriteLine(e.Message);
+            }
+        }
+
+        class Position
+        {
+            public int Line { get; set; }
+            public int Column { get; set; }
+
+            public Position(int line, int column)
+            {
+                Line = line;
+                Column = column;
+            }
+
+            public override string ToString()
+            {
+                return Line + "," + Column;
+            }
+
+            public void SetValue(int line, int column)
+            {
+                Line = line;
+                Column = column;
+            }
+        }
+
+        class MazeComponent
+        {
+            public Position Position { get; set; }
+            public Maze Maze { get; protected set; }
+            public string ComponentValue { get; protected set; }
+            public bool Visited { get; protected set; }
+
+            public MazeComponent(Position position, Maze maze, string componentValue)
+            {
+                Position = position;
+                Maze = maze;
+                ComponentValue = componentValue;
+                Visited = false;
+            }
+
+            public void SetVisited()
+            {
+                Visited = true;
+            }
+
+            private bool CanMove(Position pos)
+            {
+                MazeComponent aux = Maze.GetComponent(pos);
+                return aux.ComponentValue.Equals("0") && !aux.Visited;
+            }
+
+            public Dictionary<string, bool> PossibleMovements()
+            {
+                Dictionary<string, bool> aux = new Dictionary<string, bool>();
+                Position pos = new Position(0, 0);
+
+                // testa se pode deslocar para baixo
+                pos.SetValue((Position.Line + 1), Position.Column);
+                aux.Add("B", (pos.Line) > 0 && CanMove(pos));
+                // testa se pode deslocar para direita
+                pos.SetValue(Position.Line, (Position.Column + 1));
+                aux.Add("D", (pos.Column) > 0 && CanMove(pos));
+                // testa se pode deslocar para esquerda
+                pos.SetValue(Position.Line, (Position.Column - 1));
+                aux.Add("E", (pos.Column) > 0 && CanMove(pos));
+                // testa se pode deslocar para cima
+                pos.SetValue((Position.Line - 1), Position.Column);
+                aux.Add("C", (pos.Line > 0 && CanMove(pos)));
+                
+                return aux;
+            }
+        }
+
+        class MazeException : Exception
+        {
+            public MazeException(string msg) : base (msg)
+            {
+
+            }
+        }
+
+        class Maze
+        {
+            public int Lines { get; private set; }
+            public int Columns { get; private set; }
+            private MazeComponent[,] MazeComponents;
+
+            public Maze(int lines, int columns)
+            {
+                Lines = lines;
+                Columns = columns;
+                MazeComponents = new MazeComponent[lines + 1, columns + 1];
+            }
+
+            public void AddComponent(Position position, MazeComponent mazeComponent)
+            {
+                MazeComponents[position.Line,position.Column] = mazeComponent;
+            }
+
+            public MazeComponent GetComponent(Position position)
+            {
+                return MazeComponents[position.Line, position.Column];
+            }
+        }
+
+        class MazeChallenge
+        {
+            public Position CurrentPosition { get; private set; }
+            public Maze Maze { get; set; }
+            public bool ExitFound { get; private set; }
+            public List<string> VisitedPositions { get; private set; }
+            private List<string> AuxReturn { get; set; }
+
+            public MazeChallenge(Maze maze)
+            {
+                Maze = maze;
+                CurrentPosition = null;
+                ExitFound = false;
+                VisitedPositions = new List<string>();
+                AuxReturn = new List<string>();
+            }
+
+            public void IdentifyOriginPosition()
+            {
+                // percorre todo o labirinto verificando se existe algum ponto de origem (X)
+                for (int l = 1; l <= Maze.Lines; l++)
+                {
+                    for (int c = 1; c <= Maze.Columns; c++)
+                    {
+                        if (Maze.GetComponent(new Position(l, c)).ComponentValue.Equals("X"))
+                        {
+                            CurrentPosition = new Position(l, c);
+                            VisitedPositions.Add("O [" + CurrentPosition.Line + ", " + CurrentPosition.Column + "]");
+                        }
+                    }
+                }
+                if (CurrentPosition == null)
+                {
+                    throw new MazeException("O labirinto não possui nenhum ponto de origem (X).");
+                }
+            }
+
+            public void ExecuteMovement()
+            {
+                // verifica as possibilidades de se movimentar a partir da posição atual
+                Dictionary<string, bool> aux = Maze.GetComponent(CurrentPosition).PossibleMovements();
+                int countPossibleMovements = 0;
+                string movement = "";
+                foreach(var item in aux)
+                {
+                    if (item.Value)
+                    {
+                        countPossibleMovements++;
+                        movement = item.Key;
+                    }
+                }
+                // movimenta seguindo a ordem de prioridade
+                if (countPossibleMovements > 0)
+                {
+                    UpdateCurrentPosition(movement, false);
+                } else
+                {
+                    // retornar usando o mesmo caminho utilizado até este ponto “sem - saída” até o último ponto onde teve mais de uma posição possível de movimento
+                    movement = AuxReturn.Last().Substring(0, 1);
+                    string[] vector3 = AuxReturn.Last().Substring(3, 4).Split(',');
+                    CurrentPosition.Line = int.Parse(vector3[0]);
+                    CurrentPosition.Column = int.Parse(vector3[1].Trim());
+                    AuxReturn.Remove(AuxReturn.Last());
+                    UpdateCurrentPosition(movement, true);
+                }
+                // verifica se encontrou a saída do labirinto
+                if(CurrentPosition.Column == Maze.Columns || CurrentPosition.Column == 1 || CurrentPosition.Line == Maze.Lines || CurrentPosition.Line == 1)
+                {
+                    ExitFound = true;
+                }
+            }
+
+            private void UpdateCurrentPosition(string movement, bool returning)
+            {
+                // movimenta-se no labirinto seguindo as regras
+                switch (movement)
+                {
+                    case "C":
+                        if (returning)
+                        {
+                            movement = "B";
+                            CurrentPosition.Line++;
+                        } else
+                        {
+                            CurrentPosition.Line--;
+                        }
+                        break;
+                    case "E":
+                        if (returning)
+                        {
+                            movement = "D";
+                            CurrentPosition.Column++;
+                        } else
+                        {
+                            CurrentPosition.Column--;
+                        }
+                        break;
+                    case "D":
+                        if (returning)
+                        {
+                            movement = "E";
+                            CurrentPosition.Column--;
+                        }
+                        else
+                        {
+                            CurrentPosition.Column++;
+                        }
+                        break;
+                    case "B":
+                        if (returning)
+                        {
+                            movement = "C";
+                            CurrentPosition.Line--;
+                        } else
+                        {
+                            CurrentPosition.Line++;
+                        }
+                        break;
+                }
+                // seta e guarda a posição que foi visitada
+                VisitedPositions.Add(movement + " [" + CurrentPosition.Line + ", " + CurrentPosition.Column + "]");
+                Maze.GetComponent(CurrentPosition).SetVisited();
+                // caso seja uma posição que ainda não foi visitada, registra a ocorrência em uma lista que auxiliará a retornar pelo caminho (quando for o caso)
+                if (!returning)
+                {
+                    AuxReturn.Add(movement + " [" + CurrentPosition.Line + ", " + CurrentPosition.Column + "]");
+                }
             }
         }
     }
